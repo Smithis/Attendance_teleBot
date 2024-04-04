51ffb13a71866c7366592c61874d95698ff0d648
Smithis <88709267+Smithis@users.noreply.github.com>
2024-04-04 11:03:56 -0400

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,43 @@
+import telegram
+from king import home
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Define a function to handle the /start command
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hello! Enter the username.')
+
+# Define a function to handle user messages
+def echo(update: Update, context: CallbackContext):
+    d=home(update.message.text)
+    update.message.reply_text(d)
+    
+def display(update: Update, context: CallbackContext):
+    with open('users.txt','r') as f:
+        d=f.readlines()
+
+    update.message.reply_text(d)
+    
+    
+
+def main() -> None:
+    # Create the Updater and pass it your bot's token
+    updater = Updater("6441353473:AAGGupzFm0Aveq-XTwmVCHIa_XvdWzJO0sw")
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Register command handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("display", display))
+    # Register a message handler
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
---
+++ king.py
@@ -0,0 +1,63 @@
+import re
+import json
+import time
+import requests
+
+
+def cooki():  
+    try:
+        q=requests.get("http://login.sreyas.ac.in:80/authcheck.aspx",allow_redirects=False)	
+        cook=q.cookies.get_dict()
+        q=requests.get("http://login.sreyas.ac.in:80/default.aspx",allow_redirects=False,cookies=cook)
+        burp0_data = {"__VIEWSTATE": q.text[1759:2047], "__VIEWSTATEGENERATOR": "CA0B0334", "__EVENTVALIDATION": q.text[2227:2315], "txtId1": '', "txtPwd1": '', "txtId2": "21ve1a6680", "txtPwd2": "webcap", "imgBtn2.x": "40", "imgBtn2.y": "4"}
+        q=requests.post("http://login.sreyas.ac.in:80/default.aspx",cookies=cook,data=burp0_data,allow_redirects=False)
+        with open("keys.txt","w") as f:
+            f.write(cook['ASP.NET_SessionId']+'\n'+q.cookies['frmAuth'])
+    except:
+        return "retry"
+
+def getAttendance(roll):
+    try:
+        with open('keys.txt','r') as f:
+            key=f.read().split('\n')
+        url = "http://login.sreyas.ac.in:80/ajax/StudentAttendance,App_Web_studentattendance.aspx.a2a1b31c.ashx?_method=ShowAttendance&_session=no"
+        cookiess = {"ASP.NET_SessionId":key[0], "frmAuth": key[1]}
+        data = f"""rollNo={roll}
+fromDate=
+toDate=
+excludeothersubjects=false"""
+        q=requests.post(url, cookies=cookiess, data=data)
+        t=q.content[:9]
+        if(t==b'\r\n\r\n<!DOC'):
+            cooki()
+            return getAttendance(roll)
+        else:
+            return q.text
+    except Exception as e:
+        return "retry"
+
+def home(roll):
+    with open("name.json",'a+') as f:
+        f.write("{},{}\n".format(roll,time.ctime(time.time())))
+    l=getAttendance(roll)
+    if(l=='retry'):
+        return "Invalid input"
+    else:
+        s=''
+    for i in l:
+          s+=i
+    l=s
+    l=l[1073::]
+    x=re.findall('>[a-zA-Z0-9% ./-]+',l)
+    l=[i.removeprefix('>') for i in x]
+    d=l[:10:]
+    p=l[10::]
+    del l
+    p.insert(-4,'17')
+    data=""
+    for i in range(0,10,2):
+        data+=f'{d[i]}  : {d[i+1]} \n'
+            
+    data+=f'Total  : {p[(5*11)+4]}'
+    print(data)
+    return data
